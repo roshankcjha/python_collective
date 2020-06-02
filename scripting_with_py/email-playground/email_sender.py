8c6ca6f9e88cebeb3cba2ec4d17f6ea9698cd09b
Roshan Krishnachandra Jha <roshanjha819@gmail.com>
2020-06-02 19:22:56 +0530

    successfull execution in cmd task

--- scripting_with_py/email-playground/email_sender.py
+++ scripting_with_py/email-playground/email_sender.py
@@ -24,4 +24,20 @@ with smtplib.SMTP(host='email_smtp', port=587) as smtp:
 
 # Certain tasks to do before executing this task successfully:
 # sender_mail_id,sender_mail_password,email_smtp,receiver_email_id should be replaced with actual mails
-# You can use the SMTP of other mails .All you need to do is to search for them on the Net and understand their syntax of how to use them
+
+# You can use the SMTP of other mails .
+# All you need to do is to search for them on the Net and understand their syntax of how to use them
+
+# Heads up! If you are following along(and using google Gmail account)
+# a recent Google update to their terms and features means
+# you have to do an extra step to be able to send emails.
+
+# In the case that you are sending emails through GMAIL,
+# just go to your account(gmail) -> Account setting
+# -> Scroll to the bottom of the page
+# and you see Less Secured Apps tab,
+# now you just have to turn that feature ON.
+
+# for more info visit this:
+# Links Less Secured Apps: https: // support.google.com/accounts/answer/6010255
+# Third party sites & apps: https: // support.google.com/accounts/answer/3466521
